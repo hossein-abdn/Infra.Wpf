7c0dfdf968c973edd87d5e310b29a0fd6b7b203e
Hossein-abdn <hossein.abdn@gmail.com>
2017-11-27 12:56:07 +0330

    For Test

--- TestControls/MainWindow.xaml.cs
+++ TestControls/MainWindow.xaml.cs
@@ -12,22 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Infra.Wpf.Controls;
-using System.Globalization;
-using System.Threading;
-using Infra.Wpf;
-using C1.WPF;
-using System.Windows.Forms;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Collections;
-using Infra.Wpf.Business;
-using System.Linq.Expressions;
-using Infra.Wpf.Common.Helpers;
-using Infra.Wpf.Repository;
-using System.Data.Entity;
-using Infra.Wpf.Mvvm;
 
 namespace TestControls
 {
@@ -36,26 +20,9 @@ namespace TestControls
     /// </summary>
     public partial class MainWindow : Window
     {
-        public RelayCommand<string> GetAllCommand { get; set; }
-
-        private static void Execute(string obj)
-        {
-            int a = 1;
-        }
-
         public MainWindow()
         {
-            GetAllCommand = new RelayCommand<string>(Execute);
-
             InitializeComponent();
-
-            DataContext = new StudentVm { Model = new DataAccess.Models.Person() };
-
-            CultureInfo calture = new CultureInfo("fa-IR");
-            calture.NumberFormat.DigitSubstitution = DigitShapes.NativeNational;
-            Thread.CurrentThread.CurrentCulture = calture;
-            Thread.CurrentThread.CurrentUICulture = calture;
-            
         }
     }
 }
